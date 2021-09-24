f124c9e9ff25f03c9c17525a8baa2133ff284c07
Piotr Dudko <piotr.dudko@cloudbest.com>
2021-09-24 14:38:43 +0200

    Create an example SushiLPNFT contract

--- scripts/deploy-aave-lpnft.ts
+++ scripts/deploy-aave-lpnft.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat";
-import { DojoNFT, LPNFT } from "../typechain";
+import { AaveLPNFT, DojoNFT } from "../typechain";
 import { deployContract, waitForReceipt, deployAaveContracts } from "../utils/deployment";
 
 const nftTokenId = 0;
@@ -14,7 +14,7 @@ async function main() {
   await dojoNft.mint().then(waitForReceipt);
   console.log("Minted NFT token");
   
-  const lpnft = await deployContract<LPNFT>("LPNFT", [aToken.address, dojoNft.address]);
+  const lpnft = await deployContract<AaveLPNFT>("AaveLPNFT", [aToken.address, dojoNft.address]);
 
   const TOKENS_IN_LPNFT = 500000;
   await underlyingToken.approve(aaveLendingPoolCoreAddress, TOKENS_IN_LPNFT).then(waitForReceipt);
--- scripts/deploy-sushi-lpnft.ts
+++ scripts/deploy-sushi-lpnft.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat";
-import { TokenERC20, UniswapV2Factory, UniswapV2Pair, UniswapV2Router02, WETH9Mock } from "../typechain";
+import { DojoNFT, SushiLPNFT, TokenERC20, UniswapV2Factory, UniswapV2Pair, UniswapV2Router02, WETH9Mock } from "../typechain";
 import { deployContract, waitForReceipt } from "../utils/deployment";
 
 
@@ -24,41 +24,34 @@ async function main() {
   const pairAddress = await uniswapv2SushiFactory.getPair(token1.address, token2.address); 
   console.log(`Pair address: ${pairAddress.toString()}`);
 
-  let latestBlock = await ethers.provider.getBlock("latest");
-  let latestBlockTime = latestBlock.timestamp;
+  const latestBlock = await ethers.provider.getBlock("latest");
+  const latestBlockTime = latestBlock.timestamp;
+
+  const TOKENS_IN_LPNFT = 100000;  
   await Promise.all([
-    token1.approve(pairAddress, 100000).then(waitForReceipt),
-    token2.approve(pairAddress, 100000).then(waitForReceipt),
-    token1.approve(uniswapv2SushiRouter.address, 100000).then(waitForReceipt),
-    token2.approve(uniswapv2SushiRouter.address, 100000).then(waitForReceipt),
+    token1.approve(uniswapv2SushiRouter.address, TOKENS_IN_LPNFT).then(waitForReceipt),
+    token2.approve(uniswapv2SushiRouter.address, TOKENS_IN_LPNFT).then(waitForReceipt),
   ]);
 
+  await uniswapv2SushiRouter.addLiquidity(token1.address, token2.address, TOKENS_IN_LPNFT, TOKENS_IN_LPNFT, 0, 0, owner.address, latestBlockTime + 100000).then(waitForReceipt);
   console.log(`Token1 balance after adding liquidity ${await token1.balanceOf(owner.address)}, token2: ${await token2.balanceOf(owner.address)}`);
-  await uniswapv2SushiRouter.addLiquidity(token1.address, token2.address, 100000, 100000, 0, 0, owner.address, latestBlockTime + 100000).then(waitForReceipt);
 
   const pair = await ethers.getContractAt<UniswapV2Pair>("UniswapV2Pair", pairAddress);
-  
-  const token1inPair = await token1.balanceOf(pair.address);
-  const token2inPair = await token2.balanceOf(pair.address);
   const liquidity = await pair.balanceOf(owner.address);
-  const totalSupply = await pair.totalSupply();
-  const naiveToken1Expected = token1inPair.mul(liquidity).div(totalSupply);
-  const naiveToken2Expected = token2inPair.mul(liquidity).div(totalSupply);
   console.log(`Balance of SLP after adding liquidity: ${liquidity}`); 
 
-  await pair.approve(uniswapv2SushiRouter.address, 100000).then(waitForReceipt);
+  const dojoNft = await deployContract<DojoNFT>("DojoNFT", []);
+  await dojoNft.mint().then(waitForReceipt);
+  console.log("Minted NFT token");
 
-  latestBlock = await ethers.provider.getBlock("latest");
-  latestBlockTime = latestBlock.timestamp;
-  await uniswapv2SushiRouter.removeLiquidity(
-    token1.address,
-    token2.address,
-    liquidity,
-    naiveToken1Expected,
-    naiveToken2Expected,
-    owner.address,
-    latestBlockTime + 100000,
-  ).then(waitForReceipt);
+  const lpnft = await deployContract<SushiLPNFT>("SushiLPNFT", [pair.address, uniswapv2SushiRouter.address, dojoNft.address]);
+
+  await pair.approve(lpnft.address, liquidity).then(waitForReceipt);
+  await lpnft.addLPtoNFT(0, liquidity).then(waitForReceipt);
+  console.log(`Moving LP to NFT done, liquidity left: ${await pair.balanceOf(owner.address)}`);
+
+  await lpnft.redeemLPTokens(0, liquidity).then(waitForReceipt);
+  console.log(`Token1 balance after removing liquidity ${await token1.balanceOf(owner.address)}, token2: ${await token2.balanceOf(owner.address)}`);
 }
 
 main()
