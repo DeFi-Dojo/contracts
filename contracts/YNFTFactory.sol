773de43001b47858a2f22eca4684fc03ef0bcb3d
Paweł Kędzia <pawel.kedzia@minebest.com>
2021-11-08 11:57:18 +0100

    Working swap

--- contracts/YNFTFactory.sol
+++ contracts/YNFTFactory.sol
@@ -4,38 +4,55 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./sushiswap/uniswapv2/interfaces/IUniswapV2Router02.sol";
+// import "./sushiswap/uniswapv2/interfaces/IUniswapV2Factory.sol";
 import "./YNFT.sol";
 
 
 contract YNFTFactory {
     YNFT public immutable yNFT;
     IUniswapV2Router02 public immutable dexRouter;
-    address private daiAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    IERC20 public immutable DAI;
+    // IUniswapV2Factory public immutable override factory;
+
+    address private daiAddress = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;
 
     constructor(address _dexRouter) {
         yNFT = new YNFT();
         dexRouter = IUniswapV2Router02(_dexRouter);
+        DAI = IERC20(daiAddress);
+        // factory = IUniswapV2Factory(dexRouter.factory);
     }
 
-    function createYNFT(address user) public payable {
+    function createYNFT(address user, uint _amountIn, uint _amountOutMin) public {
         uint256 tokenId = yNFT.mint(user);
+
+        require(DAI.transferFrom(msg.sender, address(this), _amountIn), 'transferFrom failed.');
+
         uint deadline = block.timestamp + 15; // using 'now' for convenience, for mainnet pass deadline from frontend!
-        uint[] memory amounts = dexRouter.swapETHForExactTokens{ value: msg.value }(getEstimatedDAIforETH(msg.value)[0], getPathForDAItoETH(), address(this), deadline);
+        address[] memory path = new address[](2);
+        path[0] = daiAddress;
+        path[1] = dexRouter.WETH();
 
-        // refund leftover ETH to user
-        (bool success,) = msg.sender.call{ value: address(this).balance }("");
-        require(success, "refund failed");
+        require(DAI.approve(address(dexRouter), _amountIn), 'approve failed.');
+
+        dexRouter.swapExactTokensForETH(_amountIn, _amountOutMin, path, user, deadline);
     }
 
-    function getEstimatedDAIforETH(uint ethAmount) public view returns (uint[] memory amounts) {
-        // return dexRouter.getAmountsIn(ethAmount, getPathForDAItoETH());
-    }
+    // function getAmountOutMin(uint256 _amountIn) external view returns (uint256) {
 
-  function getPathForDAItoETH() public view returns (address[] memory) {
-    address[] memory path = new address[](2);
-    path[0] = dexRouter.WETH();
-    path[1] = daiAddress;
-    return path;
-  }
+    //     address[] memory path = new address[](2);
+    //     path[0] = daiAddress;
+    //     path[1] = dexRouter.WETH();
+    //     uint256[] memory amountOutMins = dexRouter.getAmountsOut(_amountIn, path);
+    //     return amountOutMins[path.length -1];
+    // }
+
+  // function getPathForDAItoETH() public view returns (address[] memory) {
+  //   address[] memory path = new address[](2);
+  //   path[0] = dexRouter.WETH();
+  //   path[1] = daiAddress;
+  //   return path;
+  // }
 }
--- scripts/deploy-ynft.ts
+++ scripts/deploy-ynft.ts
@@ -11,16 +11,21 @@ const NFT_TOKEN_ID = 0;
 
 // const A_WETH_ADDRESS_KOVAN = "0x87b1f4cf9BD63f7BBD3eE1aD04E8F52540349347";
 const A_WETH_ADDRESS = "0xF45444171435d0aCB08a8af493837eF18e86EE27";
+
+const ROUTER_DEX_KOVAN_SUSHISWAP = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+
 const ROUTER_DEX = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
 
-const FACTORY_ADDRESS = "0x9F8d4c2D6f391A8E626901c7C4bC3c7fBB2415E0";
+const FACTORY_ADDRESS = "0x79B97109961C8247055ef8A9B2F7549966F66C57";
+
+const DAI_KOVAN_ADDRESS = "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa";
 
 async function main() {
   const [owner] = await ethers.getSigners();
   console.log(`Deploying contracts using address: ${owner.address}`);
 
   const yNFTFactory = await deployContract<YNFTFactory>("YNFTFactory", [
-    ROUTER_DEX,
+    ROUTER_DEX_KOVAN_SUSHISWAP,
   ]);
 
   // await yNFTFactory
@@ -29,13 +34,37 @@ async function main() {
   //   })
   //   .then(waitForReceipt);
 
-  await yNFTFactory.createYNFT(owner.address).then(waitForReceipt);
+  // const YNFTFactoryContract = await ethers.getContractFactory("YNFTFactory");
+
+  // const yNFTFactory = await YNFTFactoryContract.attach(FACTORY_ADDRESS);
+
+  const amountIn = BigInt(10 * 10 ** 18);
+
+  const amountOutMin = BigInt(4142570000000000);
+
+  const DAI = await ethers.getContractFactory("TokenERC20");
+
+  const dai = await DAI.attach(DAI_KOVAN_ADDRESS);
+
+  await dai.approve(yNFTFactory.address, amountIn).then(waitForReceipt);
+
+  console.log("APPROVED");
+
+  await yNFTFactory
+    .createYNFT(owner.address, amountIn, amountOutMin)
+    .then(waitForReceipt);
+
+  console.log("CREATED");
 
   // const YNFTFactoryContract = await ethers.getContractFactory("YNFTFactory");
 
   // const yNFTFactory = await YNFTFactoryContract.attach(FACTORY_ADDRESS);
 
-  // const amount = await yNFTFactory.getEstimatedDAIforETH(1);
+  // const amount = await yNFTFactory.getAmountOutMin(1);
+
+  // console.log("amount");
+
+  // console.log(amount.toString());
 
   // console.log(amount.toString());
 }
