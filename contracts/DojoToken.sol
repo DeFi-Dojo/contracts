751c008e2c8062f33726a111071a1796596d28a5
mgrychow <maciej.grychowski@onet.eu>
2021-12-10 17:59:52 +0100

    ERC20 token contract

---
+++ contracts/DojoToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DojoToken is ERC20 {
+    string name_ = "$DOJO Token";
+    string symbol_ = "DOJO";
+    uint256 supply_ = 400000000;
+
+    constructor() ERC20(name_, symbol_)
+    {
+        _mint(msg.sender, supply_);
+    }
+}
