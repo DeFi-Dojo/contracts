ac341a4d2615ed592f31f5f99ddaa34157b6fb29
Paweł Kędzia <pawel.kedzia@minebest.com>
2021-12-28 12:06:07 +0100

    Refactor

--- contracts/yNFTVaults/QuickswapYNFTVault copy.sol
+++ contracts/yNFTVaults/QuickswapYNFTVault copy.sol
@@ -2,13 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "../interfaces/uniswapv2/IUniswapV2Router02.sol";
 import "../interfaces/uniswapv2/IUniswapV2Pair.sol";
 import "../interfaces/quickswap/IStakingDualRewards.sol";
 import "../interfaces/quickswap/IStakingRewards.sol";
