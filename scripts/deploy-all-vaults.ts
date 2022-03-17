b9f90e8db4db27d9c2e25032b4e30d1e7ce817ff
stpoa <stpoaa+dev@gmail.com>
2022-03-17 10:29:19 +0100

    feat: create scripts for prod vaults deployment

---
+++ scripts/deploy-all-vaults.ts
@@ -0,0 +1,16 @@
+import {
+  deployAaveVaultsWithMetadata,
+  deployQuickswapVaultsWithMetadata,
+} from "../utils";
+
+const main = async () => {
+  await deployAaveVaultsWithMetadata();
+  await deployQuickswapVaultsWithMetadata();
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- utils/deployment/index.ts
+++ utils/deployment/index.ts
@@ -2,3 +2,4 @@ export * from "./deployment";
 export * from "./aaveDummyVaults";
 export * from "./aaveVaults";
 export * from "./quickswapVaults";
+export * from "./quickswapDummyVaults";
