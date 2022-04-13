c146f28c8f22f3466f2ffde536270fa02e255c56
stpoa <stpoaa+dev@gmail.com>
2022-04-13 11:28:44 +0200

    feat: add aave vault addresses

--- consts/deployed/index.ts
+++ consts/deployed/index.ts
@@ -1,5 +1,6 @@
-import { QuickswapVaultName } from "../consts";
+import { AaveVaultName, QuickswapVaultName } from "../consts";
 import deployedVaultsQuickswap from "./vaults-quickswap.json";
+import deployedVaultsAave from "./vaults-aave.json";
 
 type DeployedVaultQuickswap = { vault: string; ynft: string };
 
@@ -11,5 +12,18 @@ export const getDeployedVaultsQuickswap = (): DeployedVaultsQuickswap =>
   deployedVaultsQuickswap;
 
 export const getDeployedVaultQuickswap = (
+  vault: QuickswapVaultName
+): DeployedVaultQuickswap => deployedVaultsQuickswap[vault];
+
+type DeployedVaultAave = { vault: string; ynft: string };
+
+type DeployedVaultsAave = {
+  [k in AaveVaultName]: DeployedVaultAave;
+};
+
+export const getDeployedVaultsAave = (): DeployedVaultsAave =>
+  deployedVaultsAave;
+
+export const getDeployedVaultAave = (
   vault: QuickswapVaultName
 ): DeployedVaultQuickswap => deployedVaultsQuickswap[vault];
