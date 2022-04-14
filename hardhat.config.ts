0ea4c1f537080ea461143d7154d4f92222867f31
stpoa <stpoaa+dev@gmail.com>
2022-04-14 14:03:36 +0200

    feat: setup hardhat dodoc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
 import "hardhat-gas-reporter";
 import "@nomiclabs/hardhat-etherscan";
+import "@primitivefi/hardhat-dodoc";
 
 import { task, HardhatUserConfig } from "hardhat/config";
 import configEnv from "./config";
