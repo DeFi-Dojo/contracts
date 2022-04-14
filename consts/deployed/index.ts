d8319b0a55bc384b21297cd687274549c8227ed6
slaweks <stpoaa+dev@gmail.com>
2022-04-14 13:38:13 +0200

    feat: DEX-245 autotasks for all vaults (#70)
    * feat: make quickswap rewards getter work for all vaults
    
    * feat: make quickswap token depositor work for all vaults
    
    * feat: update vault addresses
    
    * feat: add aave vault addresses
    
    * feat: make aave harvester work for all vaults

--- consts/deployed/index.ts
+++ consts/deployed/index.ts
@@ -1,9 +1,29 @@
-import { QuickswapVaultName } from "../consts";
+import { AaveVaultName, QuickswapVaultName } from "../consts";
 import deployedVaultsQuickswap from "./vaults-quickswap.json";
+import deployedVaultsAave from "./vaults-aave.json";
+
+type DeployedVaultQuickswap = { vault: string; ynft: string };
 
 type DeployedVaultsQuickswap = {
-  [k in QuickswapVaultName]: { vault: string; ynft: string };
+  [k in QuickswapVaultName]: DeployedVaultQuickswap;
 };
 
 export const getDeployedVaultsQuickswap = (): DeployedVaultsQuickswap =>
   deployedVaultsQuickswap;
+
+export const getDeployedVaultQuickswap = (
+  vault: QuickswapVaultName
+): DeployedVaultQuickswap => deployedVaultsQuickswap[vault];
+
+type DeployedVaultAave = { vault: string; ynft: string };
+
+type DeployedVaultsAave = {
+  [k in AaveVaultName]: DeployedVaultAave;
+};
+
+export const getDeployedVaultsAave = (): DeployedVaultsAave =>
+  deployedVaultsAave;
+
+export const getDeployedVaultAave = (
+  vault: QuickswapVaultName
+): DeployedVaultQuickswap => deployedVaultsQuickswap[vault];
--- scripts/defender-autotasks/quickswap-token-depositor/src/config.ts
+++ scripts/defender-autotasks/quickswap-token-depositor/src/config.ts
@@ -1,7 +1,14 @@
 import { NETWORK_ADDRESSES, SECONDS_IN_ONE_DAY } from "../../../../consts";
+import { getDeployedVaultsQuickswap } from "../../../../consts/deployed";
 
-export const VAULT_ADDRESS = "0xa12f6ad67F8187ddAC8A102381A7a664F790725b";
 export const MATIC_CHAIN_ID = 137;
 export const DEADLINE_SECONDS = SECONDS_IN_ONE_DAY;
-export const ADDRESSES = NETWORK_ADDRESSES["MATIC"];
+export const ADDRESSES = NETWORK_ADDRESSES.MATIC;
 export const GAS_LIMIT = 1000000;
+export const VAULTS = Object.entries(getDeployedVaultsQuickswap()).map(
+  ([vaultName, vault]) => ({
+    vaultName,
+    vaultAddress: vault.vault,
+    ynftAddress: vault.ynft,
+  })
+);
