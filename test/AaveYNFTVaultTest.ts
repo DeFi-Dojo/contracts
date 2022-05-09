131f29c4ec94320bd5b391281f05cd3f5c4194b3
slaweks <stpoaa+dev@gmail.com>
2022-05-09 09:59:18 +0200

    Revert "feat: fix aave get amount to claim (#82)"
    This reverts commit e9ce4d4bd84b7e8ec72579f2ed150f35fc50c6b8.

--- test/AaveYNFTVaultTest.ts
+++ test/AaveYNFTVaultTest.ts
@@ -61,8 +61,8 @@ describe("AaveYNFTVault", () => {
 
   it("should return value from aave incentives controller on getAmountToClaim", async () => {
     const EXPECTED_AMOUNT = 200;
-    aaveIncentivesController.getUserUnclaimedRewards
-      .whenCalledWith(aaveYnftVault.address)
+    aaveIncentivesController.getRewardsBalance
+      .whenCalledWith([aToken.address], aaveYnftVault.address)
       .returns(EXPECTED_AMOUNT);
     expect(await aaveYnftVault.getAmountToClaim()).to.equal(EXPECTED_AMOUNT);
   });
