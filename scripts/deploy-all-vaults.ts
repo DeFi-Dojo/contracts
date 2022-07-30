e8f394c0df8abbbaaf85e2f586f6f3ad00d97976
slaweks <stpoaa+dev@gmail.com>
2022-07-30 11:09:21 +0200

    feat: deploy prod vaults (#105)

--- scripts/deploy-all-vaults.ts
+++ scripts/deploy-all-vaults.ts
@@ -4,8 +4,8 @@ import {
 } from "../utils";
 
 const main = async () => {
-  await deployAaveVaultsWithMetadata({});
-  await deployQuickswapVaultsWithMetadata({});
+  await deployAaveVaultsWithMetadata({ isDummyVault: false });
+  await deployQuickswapVaultsWithMetadata({ isDummyVault: false });
 };
 
 main()
